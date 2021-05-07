1b308eb34a7126f6cd60c2a368c554cb9c342651
Taritri <taritribhattacharjee@gmail.com>
2021-05-07 13:14:34 +0530

    First Cpp program with loops introduced

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"Enter x :"<<endl;
+    cin>>x;
+    cout<<"Enter y :"<<endl;
+    cin>>y;
+    while(y>=0){
+        cout<<x+y<<endl;
+        y--;
+    }
+    cout<<"Hello"<<endl;
+    return 0;
+}
